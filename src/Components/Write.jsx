3ff0f3868d77ee484171acb45c95fb540c2cdd9e
Sean Martin <sean@seanmartinstudio.com>
2022-04-06 15:09:44 -0700

    get form submit object to appear in console log

--- src/Components/Write.jsx
+++ src/Components/Write.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Container } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -9,21 +11,35 @@ const useStyles = makeStyles((theme) => ({
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
+      font: "helvetica",
+      color: "black"
+  
     },
   },
 }));
 
 const Write = () => {
 
-  const classes = useStyles();
+  const classes = useStyles()
+  const [formData, setFormData] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(event)
+  }
+
+  
 
   // console.log("moment", moment().format())
   return (
     <div>
-    <h1 className="class">Write Page</h1>
-      <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Journal Entry" variant="outlined" />
-      </form>
+    <Container>
+      <h1 className="class">Write Page</h1>
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField id="outlined-basic" label="Journal Entry" color="primary" variant="outlined"/>
+        <Button variant="outlined" color="primary" onClick={handleSubmit}>Submit</Button>
+        </form>
+    </Container>
 
     </div>
   )
