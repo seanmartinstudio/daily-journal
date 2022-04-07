18733fa89513604755d933766f0d756c66f0b754
Sean Martin <sean@seanmartinstudio.com>
2022-04-06 22:08:47 -0700

    fix 'one character off' bug in form input value

--- src/Components/Write.jsx
+++ src/Components/Write.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import moment from 'moment'
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { Container } from '@material-ui/core';
 
 
+// styling left over from material ui, figure out how to make styling
+//object for form...
 const useStyles = makeStyles((theme) => ({
 
     form: {
@@ -23,27 +22,21 @@ const Write = () => {
   const classes = useStyles()
   const [formData, setFormData] = useState({
     date: moment().format("MMM Do YY"),
-    journalEntry: "Test entry...",
+    journalEntry: "",
   })
 
+  //bug, in console log, input value is one character off...
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(event.target.value)
+    setFormData(event.target.value)
+    console.log("form data======>", formData)
   }
 
 
   return (
-    // <div>
-    //   <h1 className="class">Write Page</h1>
-    //     <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-    //     <TextField id="outlined-basic" label="Journal Entry" color="primary" variant="outlined"/><br></br>
-    //     <Button variant="outlined" color="primary" onClick={handleSubmit}>Submit</Button>
-    //     </form>
-    // </div>
-
       <form className={classes.form}>
         <label>Journal Entry:</label><br></br>
-        <input value={formData.journalEntry} />
+        <input type="text" value={formData.journalEntry} onChange={handleSubmit}/>
         <input type="submit" />
       </form>
   )
