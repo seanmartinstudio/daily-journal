0a7b507f94970328a717367af7ae76bed838b1d4
Sean Martin <sean@seanmartinstudio.com>
2022-04-13 14:16:59 -0700

    Add pseudo code to Joural Entry componenet

--- src/Components/JournalEntry.jsx
+++ src/Components/JournalEntry.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const JournalEntry = () => {
   return (
     <li>
-        
+        <h4>
+            //date
+        </h4>
+        <br></br>
+        <h3>
+            //journal entry
+        </h3>
     </li>
   )
 }
