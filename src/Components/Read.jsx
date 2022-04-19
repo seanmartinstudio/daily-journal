83594748770e19dc1fd5bc9cbf7db00e5a5788f5
Sean Martin <sean@seanmartinstudio.com>
2022-04-19 11:49:38 -0700

    Complete main branch mvp

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import DatePicker from 'react-date-picker'
 import JournalEntry from './JournalEntry'
 
 const Read = () => {
